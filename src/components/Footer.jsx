d0f4a43d0ffe6c9af16799b33eb818005a6edf94
Shreya <321ssshreya@gmail.com>
2024-10-07 00:28:36 +0530

    Footer has been made responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Box, Typography, Link, IconButton, Grid, Divider } from '@mui/material'; // Import Divider
 import { Link as RouterLink } from 'react-router-dom';
-import { Box, Typography, Link, IconButton, Grid, Divider } from '@mui/material'; // Import Divider
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
 import { FaFacebookF, FaInstagram, FaYoutube } from 'react-icons/fa';
@@ -17,9 +16,8 @@ const Footer = () => {
         padding: 4,
         marginTop: 0,
       }}
-    >
+      >
       <Grid container spacing={3}>
-        {/* ABOUT Section */}
         {/* ABOUT Section */}
         <Grid item xs={12} sm={6} md={3}>
           <Typography
@@ -28,9 +26,7 @@ const Footer = () => {
             sx={{
               fontFamily: 'Arial, Helvetica, sans-serif',
               fontWeight: 'bold',
-              fontSize: '1.5rem',
-              marginTop:'20px',
-              fontSize: '1.5rem',
+              fontSize: {xs:'1.2rem',sm:'1.5rem'},
               marginTop:'20px',
             }}
           >
@@ -47,8 +43,7 @@ const Footer = () => {
                 sx={{
                   textDecoration: 'none',
                   paddingBottom: '8px',
-                  fontSize: '1.2rem',
-                  '&:hover': { color: '#B17457' },
+                  fontSize: {xs:'1rem',sm:'1.2rem'},
                   '&:hover': { color: '#B17457' },
                 }}
               >
@@ -57,9 +52,6 @@ const Footer = () => {
             ))}
           </Box>
         </Grid>
-
-        {/* SERVICES Section */}
-
         {/* SERVICES Section */}
         <Grid item xs={12} sm={6} md={3}>
           <Typography
@@ -68,8 +60,7 @@ const Footer = () => {
             sx={{
               fontFamily: 'Arial, Helvetica, sans-serif',
               fontWeight: 'bold',
-              fontSize: '1.5rem',
-              marginTop:'20px',
+              fontSize: {xs:'1.2rem',sm:'1.5rem'},
               marginTop:'20px',
             }}
           >
@@ -86,10 +77,9 @@ const Footer = () => {
                 sx={{
                   textDecoration: 'none',
                   paddingBottom: '8px',
-                  fontSize: '1.2rem',
+                  fontSize: {xs:'1rem',sm:'1.2rem'},
                   '&:hover': { color: '#B17457' },
-                  fontSize: '1.2rem',
-                  '&:hover': { color: '#B17457' },
+          
                 }}
               >
                 {item}
@@ -99,8 +89,6 @@ const Footer = () => {
         </Grid>
 
         {/* RESOURCES Section */}
-
-        {/* RESOURCES Section */}
         <Grid item xs={12} sm={6} md={3}>
           <Typography
             variant="h6"
@@ -108,19 +96,17 @@ const Footer = () => {
             sx={{
               fontFamily: 'Arial, Helvetica, sans-serif',
               fontWeight: 'bold',
-              fontSize: '1.5rem', 
-              marginTop:'20px',
-              marginTop:'20px',
+              fontSize: {xs:'1.2rem',sm:'1.5rem'},
+              marginTop:'20px',              
             }}
           >
             RESOURCES
           </Typography>
           <Box>
             {['Blog', 'Health Tips', 'FAQs','Contact Us'].map((item, index) => (
-            {['Blog', 'Health Tips', 'FAQs','Contact Us'].map((item, index) => (
               <Link
                 key={index}
-                component={RouterLink}
+                href="#"
                 to={item.path}
                 color="grey.100"
                 display="block"
@@ -128,19 +114,15 @@ const Footer = () => {
                 sx={{
                   textDecoration: 'none',
                   paddingBottom: '8px',
-                  fontSize: '1.2rem', // Adjusted font size for the links
-                  '&:hover': { color: '#B17457' },
-                  fontSize: '1.2rem', // Adjusted font size for the links
+                  fontSize: {xs:'1rem',sm:'1.2rem'},
                   '&:hover': { color: '#B17457' },
                 }}
               >
-                {item.name}
+                {item}
               </Link>
             ))}
           </Box>
         </Grid>
-
-        {/* Logo and Hashtag Section */}
 
         {/* Logo and Hashtag Section */}
         <Grid
@@ -151,21 +133,15 @@ const Footer = () => {
           sx={{
             display: 'flex',
             flexDirection: 'column',
-            alignItems: 'flex-end', 
-            marginBottom:'25px',
-          }}
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-end', // Align items to the right
+            alignItems: {xs:'center',md:'flex-end'},
             marginBottom:'25px',
           }}
         >
-          <Box sx={{ textAlign: 'right' }}>
+          <Box sx={{ textAlign:{xs:'center', md:'right'} }}>
             <img
               src={logo}
               alt="Fitflex Logo"
-              style={{ width: '200px', marginRight:'100px' }}
+              style={{ width: '200px', marginRight:{xs:'0px',md:'100px'}}}
             />
             <Typography
               variant="body1"
@@ -173,67 +149,7 @@ const Footer = () => {
               sx={{
                 fontFamily: 'Impact, Charcoal, sans-serif',
                 fontSize: '1.4rem',
-                marginRight:'100px',
-              }}
-            >
-              #TransformWithFitFlex
-            </Typography>
-          </Box>
-        </Grid>
-      </Grid>
-
-      {/* Divider line */}
-      <Divider
-        sx={{
-          backgroundColor: 'white', // white color for the line
-          marginTop: '20px',
-          marginBottom: '25px',
-          height: '2px', // visible thickness
-        }}
-      />
-
-      {/* Social Media Icons below the divider */}
-      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
-        {[FaFacebookF, FaInstagram, faXTwitter, FaYoutube].map((Icon, index) => (
-          <IconButton
-            key={index}
-            href="#"
-            color="inherit"
-            sx={{
-              fontSize: '2rem',
-              '&:hover': { color: 'grey.500' },
-              mx: 1,
-            }}
-          >
-            {index === 2 ? <FontAwesomeIcon icon={Icon} /> : <Icon fontSize="inherit" />}
-          </IconButton>
-        ))}
-      </Box>
-
-      {/* Copyright text below the social media icons */}
-      <Typography
-        variant="body2"
-        sx={{
-          textAlign: 'center',
-          fontFamily: 'Arial, Helvetica, sans-serif',
-          marginTop: '20px',
-        }}
-      >
-        © 2024 FitFlex. All Rights Reserved.
-      </Typography>
-          <Box sx={{ textAlign: 'right' }}>
-            <img
-              src={logo}
-              alt="Fitflex Logo"
-              style={{ width: '200px', marginRight:'100px' }}
-            />
-            <Typography
-              variant="body1"
-              mt={1}
-              sx={{
-                fontFamily: 'Impact, Charcoal, sans-serif',
-                fontSize: '1.4rem',
-                marginRight:'100px',
+                marginRight:{xs:'0px',md:"100px"},
               }}
             >
               #TransformWithFitFlex
