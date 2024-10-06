20373e06ee0d715598f1556ddfbb5a49712c4821
Abhijit Motekar <109235675+AbhijitMotekar99@users.noreply.github.com>
2024-10-06 12:54:33 +0530

    Recreated and enhanced the overall Contact us form : Issue no #50. (#68)
    * Login Link breakdown bug solved
    
    * debouncing effect added
    
    * Update Register.jsx
    
    * Show password problem solved
    
    * Redesigned contact us form

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,5 +1,9 @@
-import React, { useState } from 'react';
-import emailjs from 'emailjs-com';
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+import linkedin from "../assets/img/linkedin.png";
+import github from "../assets/img/github.png";
+import gmail from "../assets/img/gmail.png";
+import phone from "../assets/img/telephone.png";
 import {
   Container,
   Box,
@@ -9,96 +13,102 @@ import {
   Paper,
   Snackbar,
   Alert,
-} from '@mui/material';
-import CheckIcon from '@mui/icons-material/Check';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { styled } from '@mui/system';
+} from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { styled } from "@mui/system";
 
 // Styled components for the contact form
 const ContactForm = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(4),
-  maxWidth: '600px',
-  margin: 'auto',
-  backgroundColor: 'rgba(0, 0, 0, 0.8)',
+  maxWidth: "600px",
+  margin: "auto",
+  backgroundColor: "rgba(0, 0, 0, 0)",
+  backdropFilter: "blur(10px)",
   borderRadius: theme.shape.borderRadius,
-  boxShadow: '0 10px 30px rgba(0, 0, 0, 0.5)',
-  transition: 'transform 0.3s ease, box-shadow 0.3s ease',
-  '&:hover': {
-    transform: 'translateY(-5px) scale(1.02)',
-    boxShadow: '0 20px 40px rgba(0, 0, 0, 0.6)',
+  boxShadow: "0 10px 30px rgba(0, 0, 0, 0.5)",
+  transition: "transform 0.3s ease, box-shadow 0.3s ease",
+  "&:hover": {
+    transform: "translateY(-5px)",
+    boxShadow: "0 20px 40px rgba(0, 0, 0, 0.6)",
   },
 }));
 
 const theme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: "dark",
     background: {
-      default: '#121212',
+      default: "#121212",
     },
     primary: {
-      main: '#ff6f61',
+      main: "#ff6f61",
     },
     secondary: {
-      main: '#ff3d00',
+      main: "#ff3d00",
     },
   },
   typography: {
-    fontFamily: 'Arial, sans-serif',
+    fontFamily: "Future2, Arial, sans-serif",
   },
 });
 
 // Background container styled
 const BackgroundContainer = styled(Box)(() => ({
-  minHeight: '100vh',
-  backgroundImage: 'linear-gradient(135deg, #1F1C2C 10%, #232526 100%)',
-  backgroundSize: 'cover',
-  backgroundPosition: 'center',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  padding: '20px',
+  minHeight: "100vh",
+  backgroundImage: "linear-gradient(135deg, #1F1C2C 10%, #232526 100%)",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: "20px",
 }));
 
 // Gradient text field styled with CheckIcon
-const GradientTextField = styled(TextField)(({ theme, error, showError, showIcon }) => ({
-  input: { color: 'white' },
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
-      borderWidth: '2px',
-      borderImageSlice: 1,
-      borderImageSource: error
-        ? 'linear-gradient(90deg, #ff3d00 0%, #ff6f61 100%)'
-        : 'linear-gradient(90deg, #56ab2f 0%, #a8e063 100%)',
+const GradientTextField = styled(TextField)(
+  ({ theme, error, showError, showIcon }) => ({
+    input: { color: "white" },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderWidth: "2px",
+        borderImageSlice: 1,
+        fontWeight: 300,
+        borderImageSource: error
+          ? "linear-gradient(90deg, rgba(255, 61, 0, 0.8) 0%, rgba(255, 111, 97, 0.8) 100%)"
+          : "linear-gradient(90deg, rgba(255, 140, 0, 0.7) 0%, rgba(241, 196, 15, 0.7) 100%)",
+      },
+      "&:hover fieldset": {
+        borderWidth: "2px",
+        borderImageSlice: 1,
+        fontWeight: 300,
+        borderImageSource: error
+          ? "linear-gradient(90deg, rgba(255, 61, 0, 0.8) 0%, rgba(255, 111, 97, 0.8) 100%)"
+          : "linear-gradient(90deg, rgba(255, 140, 0, 0.7) 0%, rgba(241, 196, 15, 0.7) 100%)",
+      },
+      "&.Mui-focused fieldset": {
+        borderWidth: "2px",
+        borderImageSlice: 1,
+        fontWeight: 300,
+        borderImageSource: error
+          ? "linear-gradient(90deg, rgba(255, 61, 0, 0.8) 0%, rgba(255, 111, 97, 0.8) 100%)"
+          : "linear-gradient(90deg, rgba(255, 140, 0, 0.7) 0%, rgba(241, 196, 15, 0.7) 100%)",
+      },
     },
-    '&:hover fieldset': {
-      borderWidth: '2px',
-      borderImageSlice: 1,
-      borderImageSource: error
-        ? 'linear-gradient(90deg, #ff3d00 0%, #ff6f61 100%)'
-        : 'linear-gradient(90deg, #56ab2f 0%, #a8e063 100%)',
+    "& .MuiFormLabel-root": {
+      color: showError ? "#ded8ce" : "white",
     },
-    '&.Mui-focused fieldset': {
-      borderWidth: '2px',
-      borderImageSlice: 1,
-      borderImageSource: error
-        ? 'linear-gradient(90deg, #ff3d00 0%, #ff6f61 100%)'
-        : 'linear-gradient(90deg, #56ab2f 0%, #a8e063 100%)',
+    "& .MuiInputAdornment-root": {
+      display: showIcon ? "block" : "none",
+      color: "green",
     },
-  },
-  '& .MuiFormLabel-root': {
-    color: showError ? '#ded8ce' : 'white',
-  },
-  '& .MuiInputAdornment-root': {
-    display: showIcon ? 'block' : 'none',
-    color: 'green',
-  },
-}));
+  })
+);
 
 const Contact = () => {
   const [open, setOpen] = useState(false);
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   const [nameError, setNameError] = useState(false);
   const [emailError, setEmailError] = useState(false);
   const [messageError, setMessageError] = useState(false);
@@ -110,16 +120,15 @@ const Contact = () => {
   const handleChange = (event) => {
     const { name, value } = event.target;
 
-    // Update state based on the input name
-    if (name === 'name') {
+    if (name === "name") {
       setName(value);
       const isValid = validateName(value);
       setShowNameIcon(isValid);
-    } else if (name === 'email') {
+    } else if (name === "email") {
       setEmail(value);
       const isValid = validateEmail(value);
       setShowEmailIcon(isValid);
-    } else if (name === 'message') {
+    } else if (name === "message") {
       setMessage(value);
       const isValid = validateMessage(value);
       setShowMessageIcon(isValid);
@@ -137,29 +146,28 @@ const Contact = () => {
       return;
     }
 
-    // Send email using EmailJS
-    emailjs.send(
-      'service_nlds6fh',
-      'template_iyudm0r',
-      {
-        from_name: name,
-        from_email: email,
-        message: message,
-      },
-      'CPgZoAFYXY-JYOWyg'
-    )
+    emailjs
+      .send(
+        "service_nlds6fh",
+        "template_iyudm0r",
+        {
+          from_name: name,
+          from_email: email,
+          message: message,
+        },
+        "CPgZoAFYXY-JYOWyg"
+      )
       .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
+        console.log("SUCCESS!", response.status, response.text);
         setOpen(true);
       })
       .catch((err) => {
-        console.error('FAILED...', err);
+        console.error("FAILED...", err);
       });
 
-    // Reset form fields
-    setName('');
-    setEmail('');
-    setMessage('');
+    setName("");
+    setEmail("");
+    setMessage("");
     setShowError(false);
     setShowNameIcon(false);
     setShowEmailIcon(false);
@@ -188,7 +196,7 @@ const Contact = () => {
   };
 
   const validateMessage = (message) => {
-    if (message.split(' ').length > 10) {
+    if (message.split(" ").length > 10) {
       setMessageError(false);
       return true;
     } else {
@@ -197,14 +205,13 @@ const Contact = () => {
     }
   };
 
-const handleClose = (event, reason) => {
-  if (reason === 'clickaway') {
-    return;
-  }
-  setOpen(false);
-  setShowError(false); // Reset showError state when closing the error alert
-};
-
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+    setShowError(false); // Reset showError state when closing the error alert
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -212,7 +219,13 @@ const handleClose = (event, reason) => {
         <Container>
           <Box sx={{ py: 5 }}>
             <ContactForm>
-              <Typography variant="h4" align="center" color='white' gutterBottom>
+              <Typography
+                variant="h4"
+                align="center"
+                color="white"
+                fontFamily={"Future2, Helvetica, Arial"}
+                gutterBottom
+              >
                 Contact Us
               </Typography>
               <form noValidate autoComplete="off" onSubmit={handleSubmit}>
@@ -230,7 +243,11 @@ const handleClose = (event, reason) => {
                     InputProps={{
                       endAdornment: showNameIcon ? <CheckIcon /> : null, // Conditionally render CheckIcon
                     }}
-                    helperText={nameError ? 'Enter full name (more than 3 characters)' : ''}
+                    helperText={
+                      nameError
+                        ? "Enter full name (more than 3 characters)"
+                        : ""
+                    }
                   />
                 </Box>
                 <Box mb={2}>
@@ -247,7 +264,9 @@ const handleClose = (event, reason) => {
                     InputProps={{
                       endAdornment: showEmailIcon ? <CheckIcon /> : null, // Conditionally render CheckIcon
                     }}
-                    helperText={emailError ? 'Please enter a valid email address.' : ''}
+                    helperText={
+                      emailError ? "Please enter a valid email address." : ""
+                    }
                   />
                 </Box>
                 <Box mb={2}>
@@ -264,7 +283,11 @@ const handleClose = (event, reason) => {
                     InputProps={{
                       endAdornment: showMessageIcon ? <CheckIcon /> : null, // Conditionally render CheckIcon
                     }}
-                    helperText={messageError ? 'Message should be more than 10 words.' : ''}
+                    helperText={
+                      messageError
+                        ? "Message should be more than 10 words."
+                        : ""
+                    }
                   />
                 </Box>
                 <Box textAlign="center">
@@ -273,25 +296,96 @@ const handleClose = (event, reason) => {
                     size="large"
                     type="submit"
                     sx={{
-                      background: nameError || emailError || messageError ? 'radial-gradient(circle at 10% 20%, rgb(238, 56, 56) 0%, rgba(206, 21, 0, 0.92) 90.1%)' : 'linear-gradient(90deg, #56ab2f 0%, #a8e063 100%)',
-                      color: nameError || emailError || messageError ? 'black' : 'white',
-                      '&:hover': {
-                        background: nameError || emailError || messageError ? 'red' : 'linear-gradient(90deg, #56ab2f 0%, #a8e063 100%)',
+                      background:
+                        "linear-gradient(90deg, rgba(255, 140, 0, 0.7) 0%, rgba(241, 196, 15, 0.7) 100%)", // Updated button gradient
+                      color: "white",
+                      "&:hover": {
+                        background:
+                          "linear-gradient(135deg, rgba(241, 196, 15, 0.7) 0%, rgba(255, 140, 0, 0.7) 100%)", // Hover gradient
+                        boxShadow: "0 0 15px rgba(255, 140, 0, 0.1)", // glow on hover
                       },
                     }}
                   >
                     Send Message
                   </Button>
+                  <Box
+                    sx={{
+                      width: "100%",
+                      height: "1px",
+                      background:
+                        "linear-gradient(90deg, #ff8c00 0%, #f1c40f 100%)",
+                      mt: 3,
+                      mb: 2,
+                    }}
+                  />
+
+                  {/* Logo links below the line */}
+                  <Box display="flex" justifyContent="center" gap={4} mb={2} mt={4}>
+                    <a
+                      href="#"
+                      className="text-gray-600 hover:text-blue-700"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        src={linkedin} // Replace with actual path to LinkedIn logo
+                        alt="LinkedIn"
+                        className="logo"
+                        style={{ height: "35px", width: "auto" }}
+                      />
+                    </a>
+                    <a
+                      href="#"
+                      className="text-gray-600 hover:text-black"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img
+                        src={github} 
+                        alt="GitHub"
+                        className="logo"
+                        style={{ height: "35px", width: "auto" }}
+                      />
+                    </a>
+                    <a href="#" className="text-gray-600 hover:text-green-600">
+                      <img
+                        src={phone} 
+                        alt="Phone"
+                        className="logo"
+                        style={{ height: "35px", width: "auto" }}
+                      />
+                    </a>
+                    <a href="#" className="text-gray-600 hover:text-red-600">
+                      <img
+                        src={gmail}
+                        alt="Email"
+                        className="logo"
+                        style={{ height: "35px", width: "auto" }} 
+                      />
+                    </a>
+                  </Box>
                 </Box>
               </form>
             </ContactForm>
             <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-              <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+              <Alert
+                onClose={handleClose}
+                severity="success"
+                sx={{ width: "100%" }}
+              >
                 Message sent successfully!
               </Alert>
             </Snackbar>
-            <Snackbar open={showError} autoHideDuration={6000} onClose={handleClose}>
-              <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+            <Snackbar
+              open={showError}
+              autoHideDuration={6000}
+              onClose={handleClose}
+            >
+              <Alert
+                onClose={handleClose}
+                severity="error"
+                sx={{ width: "100%" }}
+              >
                 Please fix the errors in the form.
               </Alert>
             </Snackbar>
