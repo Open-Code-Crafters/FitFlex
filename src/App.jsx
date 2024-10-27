cd5724c9d17171201f08053f026d43b19bc09b42
Sahil Raj Verma <sahilrv196@gmail.com>
2024-10-28 02:28:49 +0530

    Implemented Forgot Password Page (#342)

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ const Home = lazy(() => import("./views/Home.jsx"));
 const Contact = lazy(() => import("./views/Contact.jsx"));
 const Register = lazy(() => import("./views/Register.jsx"));
 const Login = lazy(() => import("./views/Login.jsx"));
+const ForgotPassword = lazy(() => import("./views/ForgotPassword.jsx")); {"forgot password location"}
 const About = lazy(() => import("./views/About.jsx"));
 const Profile = lazy(() => import("./views/Profile.jsx"));
 const Plans = lazy(() => import("./views/Plans.jsx"));
@@ -60,64 +61,62 @@ function App() {
   return (
     <>
       <Suspense fallback={<Loader />}>
-        <BlogProvider>
-          <BrowserRouter>
-            <ProgressBar />
-            <Navbar mode={mode} toggleMode={toggleMode} />
-            <Routes>
-              <Route
-                path="/"
-                element={<Home mode={mode} textcolor={textcolor} />}
-              />
-              <Route
-                path="/home"
-                element={<Home mode={mode} textcolor={textcolor} />}
-              />
-              <Route path="/contact" element={<Contact mode={mode} />} />
-              <Route
-                path="/about"
-                element={<About mode={mode} textcolor={textcolor} />}
-              />
-              <Route
-                path="/plans"
-                element={<Plans mode={mode} textcolor={textcolor} />}
-              />
-              <Route
-                path="/plans/:plansId"
-                element={<Plans mode={mode} textcolor={textcolor} />}
-              />
-              <Route
-                path="/workout/:workoutId"
-                element={<Workout mode={mode} textcolor={textcolor} />}
-              />
-              <Route path="/progress" element={<Profile />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/*" element={<NotFound />} />
-              <Route path="/healthtips" element={<HealthTips />} />
-              <Route
-                path="/blog"
-                element={<Blog mode={mode} textcolor={textcolor} />}
-              />
-              <Route
-                path="/services"
-                element={<Services mode={mode} textcolor={textcolor} />}
-              />
-              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-              <Route
-                path="/dietrecommendation"
-                element={<DietRecommendation />}
-              />
-              <Route path="/terms-of-use" element={<TermsOfUse />} />
-              <Route path="/uploadBlog" element={<UploadBlog />} />
-            </Routes>
-            <Footer />
-            <BackToTopButton />
-
-            {/* <FItFlexChatBot/> */}
-          </BrowserRouter>
-        </BlogProvider>
+      <BlogProvider>
+        <BrowserRouter>
+          <ProgressBar />
+          <Navbar mode={mode} toggleMode={toggleMode} />
+          <Routes>
+            <Route
+              path="/"
+              element={<Home mode={mode} textcolor={textcolor} />}
+            />
+            <Route
+              path="/home"
+              element={<Home mode={mode} textcolor={textcolor} />}
+            />
+            <Route path="/contact" element={<Contact mode={mode} />} />
+            <Route
+              path="/about"
+              element={<About mode={mode} textcolor={textcolor} />}
+            />
+            <Route
+              path="/plans"
+              element={<Plans mode={mode} textcolor={textcolor} />}
+            />
+            <Route
+              path="/plans/:plansId"
+              element={<Plans mode={mode} textcolor={textcolor} />}
+            />
+            <Route
+              path="/workout/:workoutId"
+              element={<Workout mode={mode} textcolor={textcolor} />}
+            />
+            <Route path="/progress" element={<Profile />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} /> {"forgot password path"}
+            <Route path="/*" element={<NotFound />} />
+            <Route path="/healthtips" element={<HealthTips />} />
+            <Route
+              path="/blog"
+              element={<Blog mode={mode} textcolor={textcolor} />}
+            />
+            <Route
+              path="/services"
+              element={<Services mode={mode} textcolor={textcolor} />}
+            />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/dietrecommendation" element={<DietRecommendation />} />
+            <Route path="/terms-of-use" element={<TermsOfUse />} />
+            <Route path="/uploadBlog" element={<UploadBlog />} />
+          </Routes>
+          <Footer />
+          <BackToTopButton />
+ 
+          {/* <FItFlexChatBot/> */}
+         </BrowserRouter>
+         </BlogProvider>
       </Suspense>
     </>
   );
