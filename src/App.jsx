aa9c5cf6f501101b7ea3a16f4335ee53dec92533
Sawan kushwah <138680328+Sawan-Kushwah@users.noreply.github.com>
2024-10-30 01:31:54 +0530

    Add/ai chatbot (#371)
    * Added animation on scrolling through out the website
    
    * Added Ai based chatbot
    
    * fixes footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Loader from "./components/Loader.jsx";
 import NotFound from "./views/NotFound.jsx";
 import BackToTopButton from "./components/BacktoTop.jsx";
 import HealthTips from "./components/healthtips.jsx"; // Import Back to Top Button
-import { color } from "framer-motion";
 import PrivacyPolicy from "./views/PrivacyPolicy.jsx";
 import TermsOfUse from "./views/Terms.jsx";
 
@@ -15,7 +14,7 @@ const Home = lazy(() => import("./views/Home.jsx"));
 const Contact = lazy(() => import("./views/Contact.jsx"));
 const Register = lazy(() => import("./views/Register.jsx"));
 const Login = lazy(() => import("./views/Login.jsx"));
-const ForgotPassword = lazy(() => import("./views/ForgotPassword.jsx")); {"forgot password location"}
+const ForgotPassword = lazy(() => import("./views/ForgotPassword.jsx")); { "forgot password location" }
 const About = lazy(() => import("./views/About.jsx"));
 const Profile = lazy(() => import("./views/Profile.jsx"));
 const Plans = lazy(() => import("./views/Plans.jsx"));
@@ -25,7 +24,7 @@ const Workout = lazy(() => import("./views/Workout.jsx"));
 const Blog = lazy(() => import("./views/Blog.jsx"));
 const Services = lazy(() => import("./views/Services.jsx"));
 
-// import FItFlexChatBot from "./components/FItFlexChatBot.jsx";
+
 import ProgressBar from "./components/ProgressBar.jsx";
 import DietRecommendation from "./components/DietRecommendation.jsx";
 import AOS from "aos";
@@ -56,67 +55,69 @@ function App() {
     AOS.init({
       offset: 80,
     });
-  }, []);
+  }, [])
+
 
   return (
     <>
       <Suspense fallback={<Loader />}>
-      <BlogProvider>
-        <BrowserRouter>
-          <ProgressBar />
-          <Navbar mode={mode} toggleMode={toggleMode} />
-          <Routes>
-            <Route
-              path="/"
-              element={<Home mode={mode} textcolor={textcolor} />}
-            />
-            <Route
-              path="/home"
-              element={<Home mode={mode} textcolor={textcolor} />}
-            />
-            <Route path="/contact" element={<Contact mode={mode} />} />
-            <Route
-              path="/about"
-              element={<About mode={mode} textcolor={textcolor} />}
-            />
-            <Route
-              path="/plans"
-              element={<Plans mode={mode} textcolor={textcolor} />}
-            />
-            <Route
-              path="/plans/:plansId"
-              element={<Plans mode={mode} textcolor={textcolor} />}
-            />
-            <Route
-              path="/workout/:workoutId"
-              element={<Workout mode={mode} textcolor={textcolor} />}
-            />
-            <Route path="/progress" element={<Profile />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} /> {"forgot password path"}
-            <Route path="/*" element={<NotFound />} />
-            <Route path="/healthtips" element={<HealthTips />} />
-            <Route
-              path="/blog"
-              element={<Blog mode={mode} textcolor={textcolor} />}
-            />
-            <Route
-              path="/services"
-              element={<Services mode={mode} textcolor={textcolor} />}
-            />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/dietrecommendation" element={<DietRecommendation />} />
-            <Route path="/terms-of-use" element={<TermsOfUse />} />
-            <Route path="/uploadBlog" element={<UploadBlog />} />
-          </Routes>
-          <Footer />
-          <BackToTopButton />
- 
-          {/* <FItFlexChatBot/> */}
-         </BrowserRouter>
-         </BlogProvider>
+        <BlogProvider>
+          <BrowserRouter>
+            <ProgressBar />
+            <Navbar mode={mode} toggleMode={toggleMode} />
+            <Routes>
+              <Route
+                path="/"
+                element={<Home mode={mode} textcolor={textcolor} />}
+              />
+              <Route
+                path="/home"
+                element={<Home mode={mode} textcolor={textcolor} />}
+              />
+              <Route path="/contact" element={<Contact mode={mode} />} />
+              <Route
+                path="/about"
+                element={<About mode={mode} textcolor={textcolor} />}
+              />
+              <Route
+                path="/plans"
+                element={<Plans mode={mode} textcolor={textcolor} />}
+              />
+              <Route
+                path="/plans/:plansId"
+                element={<Plans mode={mode} textcolor={textcolor} />}
+              />
+              <Route
+                path="/workout/:workoutId"
+                element={<Workout mode={mode} textcolor={textcolor} />}
+              />
+              <Route path="/progress" element={<Profile />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} /> {"forgot password path"}
+              <Route path="/*" element={<NotFound />} />
+              <Route path="/healthtips" element={<HealthTips />} />
+              <Route
+                path="/blog"
+                element={<Blog mode={mode} textcolor={textcolor} />}
+              />
+              <Route
+                path="/services"
+                element={<Services mode={mode} textcolor={textcolor} />}
+              />
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+              <Route path="/dietrecommendation" element={<DietRecommendation />} />
+              <Route path="/terms-of-use" element={<TermsOfUse />} />
+              <Route path="/uploadBlog" element={<UploadBlog />} />
+            </Routes>
+            <Footer />
+            <BackToTopButton />
+
+
+            {/* <FItFlexChatBot/> */}
+          </BrowserRouter>
+        </BlogProvider>
       </Suspense>
     </>
   );
