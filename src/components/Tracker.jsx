c688862b8d28474fb197ee2431e970fd8424b500
anshika013 <anshikatripathi133@gmail.com>
2024-11-13 22:50:42 +0530

    Variable defaultApiKEy added

--- src/components/Tracker.jsx
+++ src/components/Tracker.jsx
@@ -15,7 +15,7 @@ const Tracker =()=> {
             const response = await fetch(`https://api.calorieninjas.com/v1/nutrition?query=${query}`, {
                 method: 'GET',
                 headers: {
-                    'X-Api-Key': '', // Directly using your API key for testing
+                    'X-Api-Key': defaultApiKey, // Directly using your API key for testing
                     'Content-Type': 'application/json',
                 },
             });
